2f30ef5dd3daabd93829ef2026de0440d3913790
Nam3001 <nguyenvn30012003@gmail.com>
2025-04-29 22:14:33 +0700

    first time optimize seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://nam3001.github.io/sikahue-next/',
+  generateRobotsTxt: true,
+  sitemapSize: 10000, // Số lượng URL tối đa mỗi sitemap, mặc định là 7000
+  changefreq: 'monthly', // Tần suất thay đổi trang
+  priority: 0.7, // Độ ưu tiên của các trang
+};
